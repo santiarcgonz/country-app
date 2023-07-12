70bbc15ae6f6daac995a5930c13b16c135855939
santiarcgonz <arcilasantiago@gmail.com>
2023-07-12 00:17:02 -0500

    Se actualiza el buscar por región

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania'
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,13 +13,18 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[]= [];
+  public regions: Region[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania' ];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService){}
 
 
-  searchByRegion(term: string){
-    console.log('Desde ByCapitalPage', {term});
-    this.countriesService.searchRegion(term).subscribe(countries=>{
+  searchByRegion(region: Region):void{
+
+    this.selectedRegion = region;
+    
+    console.log('Desde ByCapitalPage', {region});
+    this.countriesService.searchRegion(region).subscribe(countries=>{
       this.countries = countries;
     });
   }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,9 +7,10 @@ import { Subject, debounceTime } from 'rxjs';
   styles: [
   ]
 })
-export class SearchBoxComponent implements OnInit{
+export class SearchBoxComponent implements OnInit, OnDestroy{
 
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -19,13 +20,17 @@ export class SearchBoxComponent implements OnInit{
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-      this.debouncer
+      this.debouncerSuscription = this.debouncer
         .pipe(
           debounceTime(300)
         )
         .subscribe( value => {
           this.onDebounce.emit(value);
         })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
   }
 
   emitValue(value: string){
